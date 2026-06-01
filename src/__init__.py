f50a27e708760005800ff02b8510d2f89e0fbc12
Sushant Neupane <nsushaant72@gmail.com>
2026-06-01 20:27:43 +0545

    Add initial project structure with configuration and database setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from src.books.routes import books_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Server is starting up...")
+
+    await init_db()
+
+    yield
+
+    print(f"Server is shutting down...")
+
 
 version="v1"
 app = FastAPI(
     title="Bookly API",
     description="A simple API for managing books",
     version=version,
+    lifespan=life_span
 )
 
 app.include_router(books_router,prefix=f"/api/{version}/books", tags=["books"])
