6e56af114054c3f5885982396793bc51cd4d4dc2
Sushant Neupane <nsushaant72@gmail.com>
2026-06-23 17:19:00 +0545

    Implement user authentication with signup and login routes, including JWT token generation and user model updates

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.books.routes import books_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
+from src.auth.routes import auth_router
+
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -23,4 +25,5 @@ app = FastAPI(
 )
 
 app.include_router(books_router,prefix=f"/api/{version}/books", tags=["books"])
+app.include_router(auth_router,prefix=f"/api/{version}/auth", tags=["auth"])
 
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,6 +1,13 @@
+from datetime import timedelta,datetime
+import jwt
 from passlib.context import CryptContext
+from src.config import Config
+import uuid
+import logging 
 
 password_context=CryptContext(schemes=["bcrypt"])
+
+ACCESS_TOKEN_EXPIRY=3600
 
 def generate_password_hash(password:str)->str:
     hash=password_context.hash(password)
@@ -8,3 +15,31 @@ def generate_password_hash(password:str)->str:
 
 def verify_password(password:str,hash:str)->bool:
     return password_context.verify(password,hash)
+
+def create_access_token(user_data:dict,expiry:timedelta=None,refresh:bool=False):
+    payload={}
+    payload['user']=user_data
+    payload['exp']=datetime.now() + expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    payload['jti']=str(uuid.uuid4())
+    payload['refresh']=refresh
+
+
+    token= jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRET_KEY,
+        algorithm=Config.JWT_ALGORITHM
+    )
+    return token
+
+def decode_access_token(token:str)->dict:
+    try:
+        token_data=jwt.decode(
+            jwt= token,
+            key=Config.JWT_SECRET_KEY,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
+        return token_data
+    except jwt.PYJWTError as e:
+        logging.exception(e)
+        return None
+
